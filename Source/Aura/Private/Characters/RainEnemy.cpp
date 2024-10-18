506edc961aff12ccf864525a1527c135410afe87
Gkostov01 <gkostov@infinigods.com>
2024-10-19 01:55:06 +0300

    add highlight enemy class

--- Source/Aura/Private/Characters/RainEnemy.cpp
+++ Source/Aura/Private/Characters/RainEnemy.cpp
@@ -3,3 +3,10 @@
 
 #include "Characters/RainEnemy.h"
 
+void ARainEnemy::HighlightActor()
+{
+}
+
+void ARainEnemy::UnHighlightActor()
+{
+}
--- Source/Aura/Public/Characters/RainEnemy.h
+++ Source/Aura/Public/Characters/RainEnemy.h
@@ -4,14 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "Characters/RainCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "RainEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API ARainEnemy : public ARainCharacterBase
+class AURA_API ARainEnemy : public ARainCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
 };
