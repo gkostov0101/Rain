8fa272d5627e1bf14470718cb7b516ad2d8f2738
Gkostov01 <gkostov@infinigods.com>
2024-10-19 18:55:32 +0300

    add health and mana attributes

--- Source/Aura/Private/AbilitySystem/RainAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/RainAttributeSet.cpp
@@ -2,4 +2,42 @@
 
 
 #include "AbilitySystem/RainAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+URainAttributeSet::URainAttributeSet()
+{
+}
+
+void URainAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// Health
+	DOREPLIFETIME_CONDITION_NOTIFY(URainAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(URainAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+
+	// Mana
+	DOREPLIFETIME_CONDITION_NOTIFY(URainAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(URainAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void URainAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URainAttributeSet, Health, OldHealth);
+}
+
+void URainAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URainAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void URainAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URainAttributeSet, Mana, OldMana);
+}
+
+void URainAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URainAttributeSet, MaxMana, OldMaxMana);
+}
