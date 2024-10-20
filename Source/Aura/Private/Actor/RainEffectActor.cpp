a3dba9440f5e4f101e3b3416cff6858f1b2d5547
Gkostov01 <gkostov@infinigods.com>
2024-10-20 18:49:26 +0300

    improve effect actor

--- Source/Aura/Private/Actor/RainEffectActor.cpp
+++ Source/Aura/Private/Actor/RainEffectActor.cpp
@@ -3,54 +3,31 @@
 
 #include "Actor/RainEffectActor.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
-#include "AbilitySystemInterface.h"
-#include "AbilitySystem/RainAttributeSet.h"
-#include "Components/SphereComponent.h"
 
 // Sets default values
 ARainEffectActor::ARainEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
-}
-
-void ARainEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent,
-									AActor* OtherActor,
-									UPrimitiveComponent* OtherComp,
-									int32 OtherBodyIndex,
-									bool bFromSweep,
-									const FHitResult& SweepResult)
-{
-	if (IAbilitySystemInterface* ASCIntrface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const URainAttributeSet* AttributeSet = Cast<URainAttributeSet>(ASCIntrface->GetAbilitySystemComponent()->GetAttributeSet(URainAttributeSet::StaticClass()));
-		
-		URainAttributeSet* MutableRainAttributeSet = const_cast<URainAttributeSet*>(AttributeSet);
-
-		MutableRainAttributeSet->SetHealth(AttributeSet->GetHealth() + 25.0f);
-		MutableRainAttributeSet->SetMana(AttributeSet->GetMana() - 25.0f);
-		Destroy();
-	}
-}
-
-void ARainEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent,
-									AActor* OtherActor,
-									UPrimitiveComponent* OtherComp,
-									int32 OtherBodyIndex)
-{
-
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
 void ARainEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+void ARainEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	if (TargetASC == nullptr) return;
+
+	check(GameplayEffectClass);
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
 	
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ARainEffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &ARainEffectActor::EndOverlap);
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.0f, EffectContextHandle);
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
